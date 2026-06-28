abea2fc9a04437261926e3d4fcbae45bd7dadd4f
Vengesai Sikozho <vsikozho@gmail.com>
2026-06-28 15:51:34 +0200

    fix footer icons import

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,8 @@
-import Link from "next/link";
 import {
   FaFacebookF,
-  
   FaWhatsapp,
-  
   FaTiktok,
 } from "react-icons/fa";
-
 
 export default function Footer() {
   return (
@@ -23,32 +19,34 @@ export default function Footer() {
 
         {/* Social Media */}
         <div className="flex justify-center gap-6 mt-6 text-2xl">
-          <Link
+
+          <a
             href="https://www.facebook.com/people/Shekinah-Glory-Kingdom-Church/61590302136259/?rdid=e0nv67kkPUSwGNda&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F19ZNjiWqjE%2F"
             target="_blank"
+            rel="noopener noreferrer"
             className="hover:text-blue-400 transition"
           >
             <FaFacebookF />
-          </Link>
+          </a>
 
-          
-
-          <Link
+          <a
             href="https://chat.whatsapp.com/FdNOnwL1WZDEopFqm1rGsQ?mode=gi_t"
             target="_blank"
+            rel="noopener noreferrer"
             className="hover:text-green-400 transition"
           >
             <FaWhatsapp />
-          </Link>
+          </a>
 
-          
-          <Link
+          <a
             href="https://www.tiktok.com/@shekinahglorykingdomchch?_r=1&_t=ZS-97OPEBGKXIq"
             target="_blank"
+            rel="noopener noreferrer"
             className="hover:text-gray-300 transition"
           >
             <FaTiktok />
-          </Link>
+          </a>
+
         </div>
 
         <p className="mt-8 text-sm text-gray-400">
