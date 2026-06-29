8b6aca70b39d218e65aefa22423cebe8d997d4ea
Vengesai Sikozho <vsikozho@gmail.com>
2026-06-29 10:28:22 +0200

    small updates

--- components/ServiceTimes.tsx
+++ components/ServiceTimes.tsx
@@ -12,16 +12,19 @@ export default function ServiceTimes() {
           <p className="text-amber-500 mt-2">09:00 AM</p>
         </div>
          <div className="p-6 border rounded-lg">
-          <h3 className="font-bold">Monday Bible Study</h3>
+          <h3 className="font-bold">Monday - Bible Study</h3>
           <p className="text-amber-500 mt-2">06:00 PM</p>
         </div>
 
 
         <div className="p-6 border rounded-lg">
-          <h3 className="font-bold">Wednesday Prayer and Fasting</h3>
+          <h3 className="font-bold">Wednesday - Prayer and Fasting</h3>
           <p className="text-amber-500 mt-2">05:30 PM</p>
         </div>
-
+        <div className="p-6 border rounded-lg">
+          <h3 className="font-bold">Saturday - Evangelism</h3>
+          <p className="text-amber-500 mt-2">11:00 AM</p>
+        </div>
        
       </div>
     </section>
